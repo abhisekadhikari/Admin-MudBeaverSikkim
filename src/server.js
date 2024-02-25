823e957292ab83fc6c53bcb2b51e9be285688499
Abhisek Adhikari <abhisekadhikari1906@gmail.com>
2024-02-25 19:28:30 +0530

    Update server.js to use dynamic port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const { app } = require("./app")
 const { connectDb } = require("./config/db")
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDb()
         .then(() => {
             console.log("Server is running on port 3000")
